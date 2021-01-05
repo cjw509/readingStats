35c2c53ecba7373fdc85a463b62bf7641e5d651b
Christopher Wheatley <cjw509@its.york.ac.uk>
2021-01-05 11:26:49 +0000

    Updated code to use calibre export data
    Also updated code to process the data to use a tidyverse approach

--- createWorldMap.R
+++ createWorldMap.R
@@ -1,59 +1,114 @@
+library(dplyr)
+library(tidyr)
 library(sp)
 library(geojsonio)
 library(leaflet)
-library(dplyr)
 
+#read world map in
 world.dat <- geojson_read("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson",  what = "sp")
 
 #read goodreads Library data in
-book.dat <- read.csv("data/book_data.csv", stringsAsFactors = FALSE)
+calibre.dat <- read.csv("data/calibre_books.csv", stringsAsFactors = FALSE)
 
-book.dat$Nationality <- gsub("USA", "United States of America", book.dat$Nationality)
-book.dat$Nationality <- gsub("UK", "United Kingdom", book.dat$Nationality)
-book.dat$Nationality <- gsub("Serbia", "Republic of Serbia", book.dat$Nationality)
-book.dat$Nationality <- gsub("Republic of the Congo", "Republic of Congo", book.dat$Nationality)
+#split nationality column to deal with dual nationalities
+calibre.dat <- calibre.dat %>%
+  separate(X.nationality, c("primary.nat", "secondary.nat"), "-", fill = "right")
 
+#fix country names to match with world map
+calibre.dat = calibre.dat %>% 
+  mutate(primary.nat = gsub("USA", "United States of America", primary.nat), 
+         primary.nat = gsub("UK", "United Kingdom", primary.nat),
+         primary.nat = gsub("Serbia", "Republic of Serbia", primary.nat),
+         primary.nat = gsub("Republic of the Congo", "Republic of Congo", primary.nat),
+         secondary.nat = gsub("USA", "United States of America", secondary.nat),
+         secondary.nat = gsub("UK", "United Kingdom", secondary.nat),
+         secondary.nat = gsub("Serbia", "Republic of Serbia", secondary.nat),
+         secondary.nat = gsub("Republic of the Congo", "Republic of Congo", secondary.nat)
+  )
 
-#cretae dataframes of countries with counts of owned and read books
-nat.own  <- as.data.frame(table(book.dat$Nationality))
-nat.read <- as.data.frame(table(book.dat$Nationality[book.dat$Exclusive.Shelf == "read"]))
+#create dataframes of countries with counts of owned and read books
+nat.own <- calibre.dat %>% 
+  count(primary.nat) %>% 
+  rename(admin = "primary.nat", own.count = "n")
 
-#rename columns
-names(nat.own)  <- c("admin", "own.count")
-names(nat.read) <- c("admin", "read.count")
+nat.read <- calibre.dat %>% 
+  filter(X.read == "True") %>% 
+  count(primary.nat) %>% 
+  rename(admin = "primary.nat", read.count = "n") 
 
-#set country name to character instead of factor for matching purposes
-nat.own$admin  <- as.character(nat.own$admin)
-nat.read$admin <- as.character(nat.read$admin)
-
-#cretae dataframes of countries with counts of owned and read books
-nat.own  <- as.data.frame(table(book.dat$Nationality))
-nat.read <- as.data.frame(table(book.dat$Nationality[book.dat$Exclusive.Shelf == "read"]))
-
-#rename columns
-names(nat.own)  <- c("admin", "own.count")
-names(nat.read) <- c("admin", "read.count")
-
-#set country name to character instead of factor for matching purposes
-nat.own$admin  <- as.character(nat.own$admin)
-nat.read$admin <- as.character(nat.read$admin)
-
-#merge the owned and read count datframes
-nat.join <- left_join(nat.own, nat.read, by = c("admin")) 
+#merge the owned and read count datframes and set owned but not read countries to 0
+nat.join <- left_join(nat.own, nat.read, by = c("admin")) %>% 
+              mutate(read.count = replace_na(read.count, 0))
 
 #merge the owned and read count datframes
 world.dat.new <- merge(world.dat, nat.join, by.x="ADMIN", by.y="admin")
 
-world.dat.new@data$own.count[is.na(world.dat.new@data$own.count)]   <- 0
-world.dat.new@data$read.count[is.na(world.dat.new@data$read.count)] <- 0
 
-world.dat.new@data$own.bin  <- ifelse(world.dat.new@data$own.count > 0, 1, 0)
-world.dat.new@data$read.bin <- ifelse(world.dat.new@data$read.count > 0, 1, 0)
+#set countries with no owned books to 0 counts
+world.dat.new@data <- world.dat.new@data %>% 
+                        mutate(own.count = replace_na(own.count, 0)) %>% 
+                        mutate(read.count = replace_na(read.count, 0))
 
-world.dat.new@data$own.bin  <- factor(world.dat.new$own.bin, levels = c(0,1), labels = c("No", "Yes"))
-world.dat.new@data$read.bin <- factor(world.dat.new$read.bin, levels = c(0,1), labels = c("No", "Yes"))
+# add binary owned/not owned and read/not read values for each country and set as factors
+world.dat.new@data <- world.dat.new@data %>%                        
+                        mutate(own.bin = factor(ifelse(own.count > 0, 1, 0), levels = c(0, 1), labels = c("No", "Yes"))) %>% 
+                        mutate(read.bin = factor(ifelse(read.count > 0, 1, 0), levels = c(0, 1), labels = c("No", "Yes")))
 
 #remove antartica from the map to make plots look better
 world.dat.new <- world.dat.new[!world.dat.new$ADMIN== "Antarctica", ]
 
-rm(book.dat, nat.join, nat.own, nat.read, world.dat)
+rm(calibre.dat, nat.join, nat.own, nat.read, world.dat)
+
+
+pal <- colorFactor(palette = c("#8d96a3","#00798c"),
+                   domain = world.dat.new@data$own.bin)
+
+labels.owned <- sprintf(
+  "<strong>%s</strong><br/>Books Owned - %g ",
+  world.dat.new@data$ADMIN, world.dat.new@data$own.count
+) %>% lapply(htmltools::HTML)
+
+labels.read <- sprintf(
+  "<strong>%s</strong><br/>Books Read - %g ",
+  world.dat.new@data$ADMIN, world.dat.new@data$read.count
+) %>% lapply(htmltools::HTML)
+
+labels.all <- sprintf(
+  "<strong>%s</strong><br/>Books Owned - %g <br />Books Read - %g",
+  world.dat.new@data$ADMIN, world.dat.new@data$own.count, world.dat.new@data$read.count
+) %>% lapply(htmltools::HTML)
+
+
+leaflet(data = world.dat.new) %>%
+  addPolygons(fillColor = ~pal(own.bin),
+              weight = 0.5,
+              opacity = 1,
+              color = "black",
+              fillOpacity = 0.7,
+              highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
+              label = labels.owned,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")) %>%
+  addLegend(pal = pal, values = ~own.bin, opacity = 0.7, title = "Book Owned",
+            position = "bottomright")
+
+
+
+leaflet(data = world.dat.new) %>%
+  addPolygons(fillColor = ~pal(read.bin),
+              weight = 0.5,
+              opacity = 1,
+              color = "black",
+              fillOpacity = 0.7,
+              highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
+              label = labels.read,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")) %>%
+  addLegend(pal = pal, values = ~own.bin, opacity = 0.7, title = "Book Read",
+            position = "bottomright")
+
+
